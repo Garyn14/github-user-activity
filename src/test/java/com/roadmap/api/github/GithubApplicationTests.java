9737f95dce870fb673954604ce64f066e03b5f12
Garyn14 <garyn.fernando.hr@gmail.com>
2024-09-28 13:35:18 -0500

    init project

---
+++ src/test/java/com/roadmap/api/github/GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadmap.api.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
